a4042d8b2ba31ff709292f3bcdb2dfa3ecb5aec8
ilana99 <132579012+ilana99@users.noreply.github.com>
2023-10-17 15:30:37 +0200

    update page erreur

--- src/components/Erreur/erreur.js
+++ src/components/Erreur/erreur.js
@@ -1,11 +1,12 @@
 import "./erreur.css"
+import {Link} from "react-router-dom"
 
 function Erreur() {
     return (
         <div className="erreur">
             <h2>404</h2>
             <h3>Oups! La page que vous demandez n'existe pas.</h3>
-            <p>Retourner sur la page d'acceuil</p>
+            <Link to="/" >Retourner sur la page d'acceuil</Link>
         </div>
     )
 }
